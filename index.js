8db49e3ca3a4348fbeb717d307e183f7eb9ea692
AreliSilvaCortes <lauraareli291005@gmail.com>
2026-05-07 15:46:51 -0600

    camindex

--- index.js
+++ index.js
@@ -34,6 +34,58 @@ app.post('/api/proveedores/registrar', (req, res) => {
     });
 });
 
+//Mantenimiento are
+// Actualizar proveedor
+app.post('/api/actualizar/proveedor', (req, res) => {
+    const { id, nombre, rfc, telefono, correo } = req.body;
+    const sql = 'UPDATE proveedores SET nombre = ?, rfc = ?, telefono = ?, correo = ? WHERE id = ?';
+    db.query(sql, [nombre, rfc, telefono, correo, id], (err, result) => {
+        if (err) return res.status(500).send(err);
+        res.send('Proveedor actualizado exitosamente en la base de datos');
+    });
+});
+
+// Eliminar
+app.post('/api/eliminar/proveedor', (req, res) => {
+    const { id } = req.body;
+    const sql = 'DELETE FROM proveedores WHERE id = ?';
+    db.query(sql, [id], (err, result) => {
+        if (err) return res.status(500).send(err);
+        res.send('Proveedor eliminado exitosamente de la base de datos');
+    });
+});
+
+//producto
+app.post('/api/productos/registrar', (req, res) => {
+    const { nombre, descripcion, precio, stock } = req.body;
+    const sql = 'INSERT INTO productos (nombre, descripcion, precio, stock) VALUES (?, ?, ?, ?)';
+    db.query(sql, [nombre, descripcion, precio, stock], (err, result) => {
+        if (err) return res.status(500).send(err);
+        res.send('Producto registrado exitosamente en la base de datos');
+    });
+});
+
+// Actualizar producto
+app.post('/api/actualizar/producto', (req, res) => {
+    const { id, nombre, descripcion, precio, stock } = req.body;
+    const sql = 'UPDATE productos SET nombre = ?, descripcion = ?, precio = ?, stock = ? WHERE id = ?';
+    db.query(sql, [nombre, descripcion, precio, stock, id], (err, result) => {
+        if (err) return res.status(500).send(err);
+        res.send('Producto actualizado exitosamente en la base de datos');
+    });
+});
+
+// Eliminar
+app.post('/api/eliminar/producto', (req, res) => {
+    const { id } = req.body;
+    const sql = 'DELETE FROM productos WHERE id = ?';
+    db.query(sql, [id], (err, result) => {
+        if (err) return res.status(500).send(err);
+        res.send('Producto eliminado exitosamente de la base de datos');
+    });
+});
+
+
 app.listen(3000,()=>{
 console.log("Servidor en http://localhost:3000")
 console.log("Servidor")
